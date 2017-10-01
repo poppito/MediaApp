0fbc6ce593a70a74405961eeb6f24e1b5eaf4a3b
Harsh Overseer <harsh@embry.io>
2017-10-01 21:27:12 +1100

    navigation drawer works

--- app/src/main/java/com/embry/io/presentation/view/activities/MainActivity.kt
+++ app/src/main/java/com/embry/io/presentation/view/activities/MainActivity.kt
@@ -2,7 +2,9 @@ package com.embry.io.presentation.view.activities;
 
 
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import com.embry.io.R
 import com.embry.io.app.Casterly
 import com.embry.io.injection.ActivityModule
@@ -12,10 +14,15 @@ import com.embry.io.presentation.view.presenters.MainPresenter.MainViewSurface
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
+
 class MainActivity : AppCompatActivity(), MainViewSurface {
 
     @Inject
     lateinit var mPresenter: MainPresenter
+
+    private val mDrawerItems = listOf("A", "B", "C", "D")
+
+    private var actionBarDrawerToggle : ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,7 @@ class MainActivity : AppCompatActivity(), MainViewSurface {
         inject()
         mPresenter.onStart(this)
         initialiseToolbar()
+        initialiseDrawerLayout()
     }
 
     private fun inject() {
@@ -36,5 +44,14 @@ class MainActivity : AppCompatActivity(), MainViewSurface {
 
     private fun initialiseToolbar() {
         setSupportActionBar(toolbar_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_toc_black_24px)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun initialiseDrawerLayout() {
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar_main, R.string.title_activity_main, R.string.title_activity_main)
+        list_drawer.adapter = ArrayAdapter(this, R.layout.list_item_drawer, mDrawerItems)
+        drawer_layout.addDrawerListener(actionBarDrawerToggle!!)
     }
 }
